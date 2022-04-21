5d2435c83632c1165c4b9fcfafead601000e4d93
OMEN <j.manuelvaldebenito@gmail.com>
2022-04-21 00:02:27 -0400

    añadi mas comentarios y ejemplos

--- hoisting/index.js
+++ hoisting/index.js
@@ -1,3 +1,13 @@
+//el hoisting eleva las declaraciones, por eso si imprime 2, aunq se haya inicialiszado antes de declarar la variable
 a = 2
 var a
 console.log(a)
+
+//da undefined pq el hoisting en declaraciones, no en inicializaciones
+console.log(a)
+var a = 2
+
+nameOfDog('doggy')
+function nameOfDog(name) {
+  console.log(name)
+}
